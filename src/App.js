a85aadd9406c33ec2f2c4536d1ad13d2e3dacaea
Sohail Ishaque <sohail.sohailishaq@gmail.com>
2022-10-04 00:25:44 +0500

    discover and create complete

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ const App = () => {
       <About />
       <Top />
       <Collection />
-      <Discover />
       <Create />
+      <Discover />
       <Footer />
     </div>
   )
--- src/container/Create/Create.jsx
+++ src/container/Create/Create.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import images from '../../constant/images';
+
+import './Create.css';
 
 const Create = () => {
   return (
-    <div>Create</div>
+    <div className='app__create section__padding'>
+      <div className="app__create_left">
+        <div className="app__create_left_main">
+          <div className="left__main_1">
+            <img className='create__nft' src={images.gradient_1} alt="Gradient 1" />
+            <img className='profil' src={images.profil} alt="Profil Img" />
+          </div>
+          <div className="left__main_2">
+            <img className='create__nft' src={images.gradient_2} alt="Gradient 1" />
+            <img className='profil' src={images.profil} alt="Profil Img" />
+          </div>
+        </div>
+        <div className="app__create_left_child">
+            <img className='create__nft' src={images.gradient_3} alt="Gradient 1" />
+            <img className='profil' src={images.profil} alt="Profil Img" />
+        </div>
+      </div>
+      <div className="app__create_right">
+        <h1 className='h1__font2'>Create and sell your NFTs</h1>
+        <p className='p__font1'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisi ac phasellus placerat a pellentesque tellus sed egestas. Et tristique dictum sit tristique sed non. Lacinia lorem id consectetur pretium diam ut. Pellentesque eu sit blandit fringilla risus faucibus.</p>
+        <button className='button_1'>Sign Up Now</button>
+      </div>
+    </div>
   )
 }
 
-export default Create
+export default Create;
